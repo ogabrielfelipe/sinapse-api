c636fdc3fffdead053cabee2308d4692139ec191
Gabriel Felipe Vieira <ogabriel.v.felipe@gmail.com>
2024-11-28 14:01:43 -0300

    fix: removed logs

--- src/infra/database/prisma/mappers/manager/prisma-responsible-details.ts
+++ src/infra/database/prisma/mappers/manager/prisma-responsible-details.ts
@@ -10,8 +10,6 @@ export class PrismaResponsibleDetailsMapper {
     responsible: PrismaUser,
     address: PrismaAddress,
   ): ResponsibleDetails {
-    console.log(responsible, address)
-
     const responsibleDetails = ResponsibleDetails.create({
       responsibleId: new UniqueEntityID(responsible.id),
       name: responsible.name,
