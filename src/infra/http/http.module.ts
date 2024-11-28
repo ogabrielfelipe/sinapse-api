4f8ffea47ee668fe040fc4c455dd2de6ab954fc3
Gabriel Felipe Vieira <ogabriel.v.felipe@gmail.com>
2024-11-28 14:02:46 -0300

    feat: add responsibleModule and authenticationModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,32 +1,17 @@
 import { Module } from '@nestjs/common'
-import { CreateResponsibleController } from './controllers/responsible/create-responsible.controller'
-import { RegisterResponsibleUseCase } from '@/domain/manager/application/use-cases/responsible/register-responsible'
 import { DatabaseModule } from '../database/database.module'
 import { CryptographyModule } from '../cryptography/cryptography.module'
-import { EditResponsibleController } from './controllers/responsible/edit-responsible.controller'
-import { EditResponsibleUseCase } from '@/domain/manager/application/use-cases/responsible/edit-responsible'
-import { ChangePasswordResponsibleController } from './controllers/responsible/change-password-responsible.controller'
-import { ChangePasswordResponsibleUseCase } from '@/domain/manager/application/use-cases/responsible/change-password-responsible'
-import { DeleteResponsibleController } from './controllers/responsible/delete-responsible.controller'
-import { DeleteResponsibleUseCase } from '@/domain/manager/application/use-cases/responsible/delete-responsible'
-import { GetResponsiblesController } from './controllers/responsible/get-responsibles.controller'
-import { GetResponsibleByAttributesUseCase } from '@/domain/manager/application/use-cases/responsible/get-responsibles-by-attributes'
+import { ResponsibleModule } from './controllers/responsible/responsible.module'
+import { AuthenticationModule } from './controllers/authentication/authentication.module'
 
 @Module({
-  imports: [DatabaseModule, CryptographyModule],
-  controllers: [
-    CreateResponsibleController,
-    EditResponsibleController,
-    ChangePasswordResponsibleController,
-    DeleteResponsibleController,
-    GetResponsiblesController,
+  imports: [
+    DatabaseModule,
+    CryptographyModule,
+    ResponsibleModule,
+    AuthenticationModule,
   ],
-  providers: [
-    RegisterResponsibleUseCase,
-    EditResponsibleUseCase,
-    ChangePasswordResponsibleUseCase,
-    DeleteResponsibleUseCase,
-    GetResponsibleByAttributesUseCase,
-  ],
+  controllers: [],
+  providers: [],
 })
 export class HttpModule {}
